abbdef10cfc0a204f8b2bf007d9180881902dcf3
E.Souza <esthefison@gmail.com>
2026-01-27 17:46:30 -0400

    Adicionando fillable no Course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    // Um curso tem muitas aulas
+    use HasFactory;
+
+    // Entradas permitidas.
+    protected $fillable = [
+        'title',
+        'description',
+        'video_url',
+        'is_published'
+    ];
+
+    // Um curso tem muitas aulas.
     public function lessons()
     {
         return $this->hasMany(Lesson::class)->orderBy('position');
