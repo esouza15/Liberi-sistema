4a071874af0f291b792040fa8d32d3df69ee9ca9
E.Souza <esthefison@gmail.com>
2026-01-26 22:01:27 -0400

    Traduzindo Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,7 +21,7 @@ import { Head } from '@inertiajs/vue3';
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900">
-                        You're logged in!
+                        <strong>Sucesso!</strong> Bem-vindo a Liberi Cursos.
                     </div>
                 </div>
             </div>
