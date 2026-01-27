010f55fd6cf85f1d5252bfde7c38a87492ebad57
E.Souza <esthefison@gmail.com>
2026-01-27 16:35:03 -0400

    Corrigindo duplicidade no Controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -46,21 +46,6 @@ class CourseController extends Controller
         return redirect()->route('courses.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
