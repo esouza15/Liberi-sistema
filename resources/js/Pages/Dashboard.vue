fbca1bae1f8e8cdabdd9862836b3829c9806526a
E.Souza <esthefison@gmail.com>
2026-01-27 07:48:49 -0400

    mens login atual

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,7 +21,7 @@ import { Head } from '@inertiajs/vue3';
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900">
-                        <strong>Sucesso!</strong> Bem-vindo a Liberi Cursos.
+                        <strong>Sucesso!</strong> Bem-vindo à Liberi Cursos.
                     </div>
                 </div>
             </div>
