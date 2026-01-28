62f2249b6bc88bb1b8a6a7d0bd14ee69c35202ce
E.Souza <esthefison@gmail.com>
2026-01-28 09:57:39 -0400

    Implementando Smart Continue Button-02

--- resources/js/Pages/Courses/Index.vue
+++ resources/js/Pages/Courses/Index.vue
@@ -33,8 +33,8 @@ defineProps({
 
                     <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         
-                        <Link 
-                            <Link
+                        <Link> 
+                            
                             v-for="course in courses" 
                             :key="course.id" 
                             :href="course.target_route" 
