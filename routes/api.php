59fa47ab499426739c30b45902e62287280ce1f7
E.Souza <esthefison@gmail.com>
2026-01-29 17:54:46 -0400

    teste de pag sandbox-11

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
 
-// Rota que o Asaas vai chamar
+// Rota que o Asaas vai chamar https://beta.liberi-ead.com/api/webhook/asaas
 Route::post('/webhook/asaas', [WebhookController::class, 'handle']);
